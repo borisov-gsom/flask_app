37626c745373894bbf653d4b377aec6b55a38e89
borisov-gsom <st808892@gsom.spbu.ru>
2024-06-09 18:37:53 +0000

    Application is working

--- app.py
+++ app.py
@@ -1,6 +1,31 @@
-from flask import Flask
+from flask import Flask, request
+import joblib
+import numpy
+
+MODEL_PATH = 'mlmodels/model.pkl'
+SCALER_X_PATH = 'mlmodels/scaler_x.pkl'
+SCALER_Y_PATH = 'mlmodels/scaler_y.pkl'
 
 app = Flask(__name__)
+model = joblib.load(MODEL_PATH)
+sc_x = joblib.load(SCALER_X_PATH)
+sc_y = joblib.load(SCALER_Y_PATH)
+
+@app.route('/predict_price', methods = ['GET'])
+def predict():
+    args = request.args
+    floor = args.get('floor', default=-1, type=int)
+    rooms = args.get('rooms', default=-1, type=int)
+    area = args.get('area', default=-1, type=float)
+    renovation = args.get('renovation', default=-1, type=int)
+
+    x = numpy.array([floor, rooms, area, renovation]).reshape(1,-1)
+    x = sc_x.transform(x)
+    result = model.predict(x)
+    result = sc_y.inverse_transform(result.reshape(1,-1))
+
+    return str(result[0][0])
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5444, host='0.0.0.0')
